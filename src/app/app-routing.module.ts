5a4bb42078e41978201ebfb8feaee49e359f5674
alereyna81 <narc.81@hotmail.com>
2024-10-25 21:04:08 -0600

    Configuring routes 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NameInputComponent } from './name-input/name-input.component';
 
 const routes: Routes = [{ path: 'nameinput', component: NameInputComponent }];
 
